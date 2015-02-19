ac846c29e0e53cbccb9b2802ec99a32b95bde77b
David Jensen <david.lgj@gmail.com>
2015-02-19 11:16:46 +0100

    Added sass support
    serveWithStyleUrl is changed to point locally and the SCSS is re-compiled on each request so that a developer
    just needs to reload to see the changes.

--- diversity-server.js
+++ diversity-server.js
@@ -13,10 +13,12 @@ var apiFactory = require('./lib/api.js').factory;
 var render     = require('./lib/render.js');
 var deps       = require('./lib/deps.js');
 
+var sass = require('node-sass');
+
 // Start a tiny-lr server as well.
-tinylr().listen(35729, function() {
-  console.log('... Listening on 35729 ...');
-});
+//tinylr().listen(35729, function() {
+//  console.log('... Listening on 35729 ...');
+//});
 
 if (process.argv.length < 4) {
   console.log('Usage:\n     node diversity-server.js <webshop-id> <theme-id|theme-name> [<auth>]');
@@ -36,15 +38,73 @@ var api = apiFactory(webshopUid, 'sv', auth);
 app.use(bodyParser.json());
 app.use(express.static('.'));
 
+
+app.use(function (req, res, next) {
+
+  // Skip /deps, those are files
+  if (req.url.indexOf('/deps') === 0 ||
+      req.url.indexOf('/favicon.ico') === 0) {
+    return next();
+  }
+
+  // Calculate settings and fetch the theme component
+  var fetchThemeCompAndJson = function(name) {
+    return deps.updateDeps({ // Since its not under webshop components we must give an url.
+      name: name,
+      url: 'http://git.diversity.io/themes/' + name + '.git'
+      // The second updateDeps actually loads diversity.json
+    }).then(deps.updateDeps);
+  };
+
+  var tid = parseInt(themeUid, 10);
+  var defs;
+  if (isNaN(tid)) {
+    req.themeName = themeUid;
+    // Its not a number but a theme name!
+    // settings is just default from theme component.
+    fetchThemeCompAndJson(req.themeName).then(function(comps) {
+                   req.componentDefs = comps;
+                   req.themeDiversityJson = comps[req.themeName];
+                   var data = util.schemaDefaults(req.themeDiversityJson.settings);
+                   return {params: {settings: data}};
+                 }).then(function(theme) {
+                   theme.params = theme.params || {component: 'aficionado'};
+                   req.theme = theme;
+                   next();
+                 }, function(err) {  console.log('oh noes error!', err); throw err });
+  } else {
+    api('Theme.get', [tid, true]).then(function(settings) {
+      settings.params = settings.params || {component: 'aficionado'};
+
+      // So we got settings, we still need to merge with defaults from schema.
+      req.themeName = settings.params.component;
+
+      fetchThemeCompAndJson(req.themeName).then(function(comps) {
+        req.componentDefs = comps;
+        req.themeDiversityJson = defs[req.themeName];
+        return {
+          params: {
+            settings: util.schemaDefaults(req.themeDiversityJson.settings, settings.params.settings || {}),
+          }
+        };
+      });
+    }).then(function(theme) {
+      theme.params = theme.params || {component: 'aficionado'};
+      req.theme = theme;
+      next();
+    });
+  }
+});
+
 Q.all([
   //deps.updateDeps('tws-admin-schema-form').fail(function(err) {
   //  console.log(err);
   //}),
 
-  deps.updateDeps({
-    name: 'jquery.jsonrpcclient.js',
-    url: 'https://github.com/Textalk/jquery.jsonrpcclient.js.git'
-  }),
+  //deps.updateDeps({
+  //  name: 'jquery.jsonrpcclient.js',
+  //  url: 'https://github.com/Textalk/jquery.jsonrpcclient.js.git'
+  //}),
 
 
   //deps.updateDeps({
@@ -71,165 +131,162 @@ Q.all([
   //
   // });
 
+
+  // Sass it up!
+  app.get('/css/components/:component/:version/css', function(req, res) {
+    console.log('********', req.params);
+
+    var data = Object.keys(req.query).map(function(k) {
+      // FIXME: Use regexp to validate query paramaters.
+      return '$' + k + ': ' + req.query[k] + ';';
+    });
+
+    var styles = typeof req.themeDiversityJson.style === 'string' ? [req.themeDiversityJson.style] : req.themeDiversityJson.style;
+
+    // Filter out all scss files
+    var re = /.+\.scss/;
+    styles.filter(function(s) { return re.test(s); }).forEach(function(s) {
+      data.push('@import \'' + s + '\';');
+    });
+
+    var stats = {};
+    sass.render({
+      data: data.join('\n'),
+      includePaths: ['./deps/' + req.themeName],
+      success: function(result) {
+        res.set('Content-Type', 'text/css');
+        res.set('Cache-Control', 'public, max-age=300');
+        res.send(result.css);
+      },
+      error: function(err) {
+        console.log(new Date(), err);
+        res.status(500).send(err);
+      },
+      outputStyle: 'nested',
+      stats: stats
+    });
+  });
+
   app.get('/*', function(req, res, next) {
 
     // Skip /deps, those are files
     if (req.url.indexOf('/deps') === 0) {
       return next();
     }
-
-    // FIXME: unclutter this ugly mess
-    // Calculate settings and fetch the theme component
-    var fetchThemeCompAndJson = function(name) {
-      return deps.updateDeps({ // Since its not under webshop components we must give an url.
-        name: name,
-        url: 'http://git.diversity.io/themes/' + name + '.git'
-        // The second updateDeps actually loads diversity.json
-      }).then(deps.updateDeps);
-    };
-
-    var tid = parseInt(themeUid, 10);
-    var settingsPromise;
-    var themeName;
-    var themeDiversityJson;
-    var defs;
-    if (isNaN(tid)) {
-      themeName = themeUid;
-      // Its not a number but a theme name!
-      // settings is just default from theme component.
-      settingsPromise = fetchThemeCompAndJson(themeName).then(function(comps) {
-                     defs = comps;
-                     themeDiversityJson = defs[themeName];
-                     var data = util.schemaDefaults(themeDiversityJson.settings);
-                     return {params: {settings: data}};
-                   });
-    } else {
-      settingsPromise = api('Theme.get', [tid, true]).then(function(settings) {
-        settings.params = settings.params || {component: 'aficionado'};
-
-        // So we got settings, we still need to merge with defaults from schema.
-        themeName = settings.params.component;
-
-        fetchThemeCompAndJson(themeName).then(function(comps) {
-          defs = comps;
-          themeDiversityJson = defs[themeName];
-          return {
-            params: {
-              settings: util.schemaDefaults(themeDiversityJson.settings, settings.params.settings || {}),
-            }
-          };
-        });
-      });
-    }
-
     // We update dependencies and load theme each time so we always pick up changes.
     // This is for development people!
-    settingsPromise.then(function(theme) {
-      var settings  = theme.params.settings;
-      // We want to load the supplied themes components
-      var promises = [];
+    var settings  = req.theme.params.settings;
 
-      // We still might have tws-columns,tws-container etc in place of a component.
-      // therefore we can't just a shallowly check settings.component.
-      var names = [themeName];
-      util.findComponentsInSettings(settings, function(c) {
-        promises.push(deps.updateDeps(c.component));
-        names.push(c.component);
-      });
+    var defs = req.componentDefs;
 
-      return Q.all(promises).then(function(result) {
-        // Collect all diversity.json definitions
-        result.reduce(function(soFar, obj) {
-          Object.keys(obj).forEach(function(k) {
-            if (obj[k] !== undefined) {
-              soFar[k] = obj[k];
-            }
-          });
-          return soFar;
-        }, defs);
+    // We want to load the supplied themes components
+    var promises = [];
 
-        var renderList = [];
-        var templates = [];
+    // We still might have tws-columns,tws-container etc in place of a component.
+    // therefore we can't just a shallowly check settings.component.
+    var names = [req.themeName];
+    util.findComponentsInSettings(settings, function(c) {
+      promises.push(deps.updateDeps(c.component));
+      names.push(c.component);
+    });
 
-        util.findComponentsInSettings(settings, function(obj) {
-          // findComponentsInSettings goes depth first and applies children before parents
-          var def = defs[obj.component];
-          if (def.template) {
-            // FIXME: alternative templates
-            templates.push(fs.read(path.join(
-              process.cwd(),
-              deps.DEPS_FOLDER,
-              def.name,
-              def.template
-            )));
-            renderList.push(obj);
+    return Q.all(promises).then(function(result) {
+      // Collect allreq. diversity.json definitions
+      result.reduce(function(soFar, obj) {
+        Object.keys(obj).forEach(function(k) {
+          if (obj[k] !== undefined) {
+            soFar[k] = obj[k];
           }
         });
-        //console.log('And the renderlist is: ', renderList.map(function(c) {
-        //  return c.component;
-        //}));
+        return soFar;
+      }, defs);
 
-        // Load all templates
-        return Q.all(templates).then(function(templateData) {
+      var renderList = [];
+      var templates = [];
 
-          // Render mustache templates for each in the list.
-          renderList.forEach(function(obj, i) {
-
-            var c = render.createContext(webshopUid, webshopUrl);
-            c.settings = obj.settings || {};
-            c.settingsJSON = JSON.stringify(c.settings).replace(/<\/script>/g, '<\\/script>');
-            obj.componentHTML = render.renderMustache(templateData[i], c);
-          });
-
-          var context = render.createContext(webshopUid, webshopUrl);
-          var prefix = render.prefixFactory(deps.DEPS_FOLDER);
-
-          util.traverseDeps(names, defs, function(comp) {
-            if (typeof comp.style === 'string') {
-              context.styles.push(prefix(comp.name, comp.style));
-            } else if (typeof comp.style !== 'undefined') {
-              context.styles = comp.style.map(function(u) {
-                return prefix(comp.name, u);
-              }).concat(context.styles);
-            }
-
-            if (typeof comp.script === 'string') {
-              context.scripts.unshift(prefix(comp.name, comp.script));
-            } else if (typeof comp.script !== 'undefined') {
-              context.scripts = comp.script.map(function(u) {
-                return prefix(comp.name, u);
-              }).concat(context.scripts);
-            }
-
-            if (comp.angular) {
-              context.modules[comp.angular] = true;
-            }
-          });
-
-          context.settings = settings;
-
-          // Since settingsJSON is going up to the server we need to clean out redundant code.
-          util.findComponentsInSettings(settings, function(obj) {
-            delete obj.settings;
-          }, true);
-          context.settingsJSON = JSON.stringify(settings).replace(/<\/script>/g, '<\\/script>');
-
-          context.angularBootstrap = 'angular.module("tws",["' +
-                                      Object.keys(context.modules).join('","') +
-                                     '"])\n' + 'angular.bootstrap(document,["tws"])';
-          context.baseUrl = '/deps/' + themeName + '/';
-
-          // Lets render main mustache template;
-          return fs.read(path.join(
+      util.findComponentsInSettings(settings, function(obj) {
+        // findComponentsInSettings goes depth first and applies children before parents
+        var def = defs[obj.component];
+        if (def.template) {
+          // FIXME: alternative templates
+          templates.push(fs.read(path.join(
             process.cwd(),
             deps.DEPS_FOLDER,
-            themeName,
-            defs[themeName].template
-          )).then(function(template) {
-            res.send(render.renderMustache(template, context));
-          });
+            def.name,
+            def.template
+          )));
+          renderList.push(obj);
+        }
+      });
+      //console.log('And the renderlist is: ', renderList.map(function(c) {
+      //  return c.component;
+      //}));
 
+      // Load all templates
+      return Q.all(templates).then(function(templateData) {
+
+        // Render mustache templates for each in the list.
+        renderList.forEach(function(obj, i) {
+
+          var c = render.createContext(webshopUid, webshopUrl);
+          c.settings = obj.settings || {};
+          c.settingsJSON = JSON.stringify(c.settings).replace(/<\/script>/g, '<\\/script>');
+          obj.componentHTML = render.renderMustache(templateData[i], c);
         });
+
+        var context = render.createContext(webshopUid, webshopUrl);
+        var prefix = render.prefixFactory(deps.DEPS_FOLDER);
+
+        util.traverseDeps(names, defs, function(comp) {
+          if (typeof comp.style === 'string') {
+            context.styles.push(prefix(comp.name, comp.style));
+          } else if (typeof comp.style !== 'undefined') {
+            context.styles = comp.style.map(function(u) {
+              return prefix(comp.name, u);
+            }).concat(context.styles);
+          }
+
+          if (typeof comp.script === 'string') {
+            context.scripts.unshift(prefix(comp.name, comp.script));
+          } else if (typeof comp.script !== 'undefined') {
+            context.scripts = comp.script.map(function(u) {
+              return prefix(comp.name, u);
+            }).concat(context.scripts);
+          }
+
+          if (comp.angular) {
+            context.modules[comp.angular] = true;
+          }
+        });
+
+        // Filter out scss files.
+        var re = /.*\.scss$/;
+        context.styles = context.styles.filter(function(s) { return !re.test(s); });
+
+
+        context.settings = settings;
+
+        // Since settingsJSON is going up to the server we need to clean out redundant code.
+        util.findComponentsInSettings(settings, function(obj) {
+          delete obj.settings;
+        }, true);
+        context.settingsJSON = JSON.stringify(settings).replace(/<\/script>/g, '<\\/script>');
+
+        context.angularBootstrap = 'angular.module("tws",["' +
+                                    Object.keys(context.modules).join('","') +
+                                   '"])\n' + 'angular.bootstrap(document,["tws"])';
+        context.baseUrl = '/deps/' + req.themeName + '/';
+
+        // Lets render main mustache template;
+        return fs.read(path.join(
+          process.cwd(),
+          deps.DEPS_FOLDER,
+          req.themeName,
+          defs[req.themeName].template
+        )).then(function(template) {
+          res.send(render.renderMustache(template, context));
+        });
+
       });
     }).fail(function(err) {
       if (!Array.isArray(err)) {
@@ -243,7 +300,7 @@ Q.all([
   });
 
   console.log('Starting server')
-  var server = app.listen(3000, function() {
+  var server = app.listen(process.env.PORT || 3000, function() {
     console.log('Listening on port %d', server.address().port);
   });
 
